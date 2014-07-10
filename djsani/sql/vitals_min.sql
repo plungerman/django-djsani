6dec0d3b46dd0007c8d1b48f3719df19e5b0a8e6
Bender <plungerman@gmail.com>
2014-07-10 10:07:57 -0500

    new print and export options for dashboard

--- djsani/sql/vitals_min.sql
+++ djsani/sql/vitals_min.sql
@@ -1,11 +1,14 @@
 SELECT UNIQUE
     id_rec.lastname, id_rec.firstname, id_rec.id,
-    id_rec.zip, id_rec.ctry, id_rec.phone, cvid_rec.ldap_name
+    id_rec.zip, id_rec.ctry, id_rec.phone, cvid_rec.ldap_name,
+    cvid_rec.ldap_add_date
 FROM
     id_rec
 LEFT JOIN
     cvid_rec     ON  id_rec.id = cvid_rec.cx_id
 WHERE
+    id_rec.id =
+/*
     id_rec.lastname=""
-/* id_rec.id = */
+ */
 
