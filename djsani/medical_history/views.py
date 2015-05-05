01955c008a41b442340dfb4ccc31d09d823147a8
Bender <plungerman@gmail.com>
2015-05-05 11:57:31 -0500

    updated view and template for medical history form to accomodate the various form types: new, verify, update

--- djsani/medical_history/views.py
+++ djsani/medical_history/views.py
@@ -5,22 +5,20 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
-from djsani.medical_history.forms import StudentForm, AthleteForm
+from djsani.medical_history.forms import StudentMedicalHistoryForm
+from djsani.medical_history.forms import AthleteMedicalHistoryForm
 from djsani.medical_history.models import StudentMedicalHistory
 from djsani.medical_history.models import AthleteMedicalHistory
 from djsani.core.utils import get_manager
 
 from djzbar.utils.informix import get_session
+from djtools.utils.convert import str_to_class
 from djtools.utils.database import row2dict
 
-BASES = {
-    "cc_student_medical_history": StudentMedicalHistory,
-    "cc_athlete_medical_history": AthleteMedicalHistory,
-    "StudentForm":StudentForm,
-    "AthleteForm":AthleteForm
-}
-
 EARL = settings.INFORMIX_EARL
+
+import logging
+logger = logging.getLogger(__name__)
 
 @login_required
 def form(request,stype):
@@ -30,12 +28,18 @@ def form(request,stype):
     data = {}
     # student id
     cid = request.user.id
+    # model name
+    mname = "{}MedicalHistory".format(stype.capitalize())
     # form name
-    fname = "%sForm" % stype.capitalize()
+    fclass = str_to_class(
+        "djsani.medical_history.forms",
+        "{}Form".format(mname)
+    )
+    #template = "medical_history/form.html".format(stype)
     template = "medical_history/form.html"
     # check for student record(s)
     update = False
-    table = "cc_%s_medical_history" % stype
+    table = "cc_{}_medical_history".format(stype)
 
     # create database session
     session = get_session(EARL)
@@ -43,38 +47,48 @@ def form(request,stype):
     # retrieve student manager record
     manager = get_manager(session, cid)
 
-    # check to see if they already submitted this form or we
-    # have data from previous years
-    obj = session.query(BASES[table]).filter_by(college_id=cid).\
-        filter(BASES[table].current(settings.START_DATE)).first()
+    # retrieve our model and check to see if they already
+    # submitted this form or we have data from previous years
+    model = str_to_class(
+        "djsani.medical_history.models", mname
+    )
+    obj = session.query(model).filter_by(college_id=cid).\
+        filter(model.current(settings.START_DATE)).first()
+
     if obj:
-        # if current update use the xeditable form
+        # if current update then use the xeditable form
         # otherwise we have data from the previous year but
         # the student needs to verify it
         if getattr(manager, table):
             update = True
             template = "medical_history/form_update.html"
-        else:
-            template = "medical_history/form_update.html"
-            #template = "medical_history/form_verify.html"
         # put it in a dict
         init = row2dict(obj)
-    if request.method=='POST':
+    if request.method == 'POST':
         post = request.POST.copy()
-        form = BASES[fname](post)
+        form = fclass(post)
+        logger.debug(form.as_ul())
         if form.is_valid():
             data = form.cleaned_data
             data["college_id"] = cid
-            # update 'yes' responses with value from temp field
-            for n,v in data.items():
-                if v == "Yes":
-                    data[n] = post["%s_2" % n]
-            # remove temp fields
-            for n,v in data.items():
-                if n[-2:] == "_2":
-                    data.pop(n)
-            # insert
-            put_data(data,table,noquo=["college_id"])
+            # insert else update
+            if not obj:
+                # set 'yes' responses with value from temp field
+                for n,v in data.items():
+                    if v == "Yes":
+                        data[n] = post["%s_2" % n]
+                # remove temp fields
+                for n,v in data.items():
+                    if n[-2:] == "_2":
+                        data.pop(n)
+                # insert
+                s = model(**data)
+                session.add(s)
+            else:
+                # update
+                for key, value in data.iteritems():
+                    setattr(obj, key, value)
+
             # update the manager
             setattr(manager,table,True)
             session.commit()
@@ -83,12 +97,14 @@ def form(request,stype):
                 reverse_lazy("medical_history_success")
             )
         else:
-            # for use at template level with dictionary filter
-            for n,v in post.items():
-                if n[-2:] == "_2" and v:
-                    data[n[:-2]] = v
+            if not obj:
+                # for use at template level with dictionary filter
+                for n,v in post.items():
+                    if n[-2:] == "_2" and v:
+                        data[n[:-2]] = v
+
     else:
-        form = BASES[fname](initial=init, gender=request.session['gender'])
+        form = fclass(initial=init, gender=request.session.get('gender'))
     return render_to_response(
         template,
         {
