6f6cd80b1f654c19065b778e0a4be44b3da33a32
Bender <plungerman@gmail.com>
2015-05-15 14:49:37 -0500

    javascript from the djmaidez project for loading emergency contact modal

---
+++ djsani/static/js/djmaidez.js
@@ -0,0 +1,55 @@
+function GetUserType() {
+    return "student";
+}
+
+function makeDialog() {
+    $("#main_modal").dialog({
+        autoOpen: true,
+        height: 620,
+        width: 850,
+        modal: true,
+        top: 10,
+        buttons: { "Save": function(){
+            var uid = GetUserID();
+            if(isValid() && !isNaN(uid)) {
+                $.getJSON('/emergency/contact/save?callback=?', {
+                    MIS1_NAME: $("#MIS1_NAME").val(),
+                    MIS1_REL: $("#MIS1_REL").val(),
+                    MIS1_PHONE1: $("#MIS1_PHONE1").val(),
+                    MIS1_PHONE2: $("#MIS1_PHONE2").val(),
+                    MIS1_PHONE3: $("#MIS1_PHONE3").val(),
+                    MIS2_NAME: $("#MIS2_NAME").val(),
+                    MIS2_PHONE1: $("#MIS2_PHONE1").val(),
+                    MIS3_NAME: $("#MIS3_NAME").val(),
+                    MIS3_PHONE1: $("#MIS3_PHONE1").val(),
+                    ENS_SELF_CELL: $("#ENS_SELF_CELL").val(),
+                    ENS_SMS: $('input[name=ENS_SMS]:checked').val(),
+                    ENS_CARRIER: $("#ENS_CARRIER").val(),
+                    ENS_EMAIL: $("#ENS_EMAIL").val(),
+                    ICE_NAME: $("#ICE_NAME").val(),
+                    ICE_PHONE1: $("#ICE_PHONE1").val(),
+                    ICE_PHONE2: $("#ICE_PHONE2").val(),
+                    ICE_PHONE3: $("#ICE_PHONE3").val(),
+                    ICE_REL: $("#ICE_REL").val(),
+                    ICE2_NAME: $("#ICE2_NAME").val(),
+                    UserID: uid,
+                    ICE2_PHONE1: $("#ICE2_PHONE1").val(),
+                    ICE2_PHONE2: $("#ICE2_PHONE2").val(),
+                    ICE2_PHONE3: $("#ICE2_PHONE3").val(),
+                    ICE2_REL: $("#ICE2_REL").val(),
+                    DJSANI: "True"
+                });
+                // set green checkmark
+                $(".djsani").html('<i class="fa fa-check green"></i>');
+            }
+        }}
+    });
+    $( "#ens_form").accordion({
+        active:0,
+        header:'div.header',
+        fillSpace:false,
+        clearStyle:true
+    });
+    initializeHandlers();
+    initialvalidation();
+}
