b918d5981d509ce0bf8e5a15923bbfaf663c895c
Bender <plungerman@gmail.com>
2015-05-26 11:24:33 -0500

    removed old put_data() function that was replace by ORM; pass manager to _doop() private function instead of just college id (cid); moved _doop() outside of logic

--- djsani/core/utils.py
+++ djsani/core/utils.py
@@ -7,7 +7,6 @@ from djsani.medical_history.waivers.models import Sicklecell
 from djsani.insurance.models import StudentHealthInsurance
 from djsani.core.models import StudentMedicalContentType
 from djsani.core.models import StudentMedicalManager
-from djzbar.utils.informix import do_sql as do_esql
 
 from sqlalchemy.orm.session import make_transient
 from sqlalchemy import desc
@@ -28,12 +27,12 @@ def get_content_type(session, name):
     return ct
 
 
-def _doop(session, mod, cid):
+def _doop(session, mod, man):
     """
-    check for an object and duplicate it
+    check for an object and duplicate it.
     returns the new object or None
     """
-    obj = session.query(mod).filter_by(college_id=cid).\
+    obj = session.query(mod).filter_by(college_id=man.college_id).\
         order_by(desc(mod.id)).first()
 
     if obj:
@@ -42,8 +41,10 @@ def _doop(session, mod, cid):
         make_transient(obj)
         obj.id = None
         obj.created_at = None
+        # associate the new obj with the new manager
+        obj.manager_id = man.id
         session.add(obj)
-        # is this necessary?
+        # in case we need PK id
         session.flush()
     return obj
 
@@ -78,6 +79,8 @@ def get_manager(session, cid):
             # returning student
             if obj.cc_student_immunization:
                 immunization = True
+            # if sicklecell waiver, check the latest for proof,
+            # which means always True
             if obj.cc_athlete_sicklecell_waiver:
                 # fetch the latest sicklecell waiver
                 sc = session.query(Sicklecell).filter_by(college_id=cid).\
@@ -85,73 +88,22 @@ def get_manager(session, cid):
                 if sc.proof:
                     sicklecell = True
 
-            # check for insurance object
-            ins = _doop(session, StudentHealthInsurance, cid)
-            # check for student medical history
-            smh = _doop(session, StudentMedicalHistory, cid)
-            # check for athlete medical history
-            amh = _doop(session, AthleteMedicalHistory, cid)
-
         # create new manager
         manager = StudentMedicalManager(
             college_id=cid, cc_student_immunization=immunization,
             cc_athlete_sicklecell_waiver=sicklecell, sitrep=False
         )
-        # copy health insurance and medical histories
+        # add manager
         session.add(manager)
+        session.flush()
+
+        # check for insurance object
+        ins = _doop(session, StudentHealthInsurance, manager)
+        # check for student medical history
+        smh = _doop(session, StudentMedicalHistory, manager)
+        # check for athlete medical history
+        amh = _doop(session, AthleteMedicalHistory, manager)
+
         session.commit()
 
     return manager
-
-def put_data(dic,table,cid=None,noquo=[]):
-    """
-    dic:    dictionary of data
-    table:  the name of the table in the database
-    cid:    create or update
-    noquo:  a list of field names that do not require quotes
-    """
-    if cid:
-        prefix = 'UPDATE %s SET ' % table
-        for key,val in dic.items():
-            # strip quotes
-            if key not in noquo:
-                try:
-                    val = val.replace('"', '')
-                except:
-                    pass
-            # informix expects 1 or 0
-            if val == True:
-                val = 1
-            if val == False:
-                val = 0
-            prefix += '%s=' % key
-            if noquo and key in noquo:
-                prefix += '%s,' % val
-            else:
-                prefix += '"%s",' % val
-        sql = '%s WHERE college_id=%s' % (prefix[:-1],cid)
-    else:
-        prefix = 'INSERT INTO %s' % table
-        fields = '('
-        values = 'VALUES ('
-        for key,val in dic.items():
-            # strip quotes
-            if key not in noquo:
-                try:
-                    val = val.replace('"', '')
-                except:
-                    pass
-            # informix expects 1 or 0
-            if val == True:
-                val = 1
-            if val == False:
-                val = 0
-            fields +='%s,' % key
-            if noquo and key in noquo:
-                values +='%s,' % val
-            else:
-                values +='"%s",' % val
-        fields = '%s)' % fields[:-1]
-        values = '%s)' % values[:-1]
-        sql = '%s %s %s' % (prefix,fields,values)
-    do_esql(sql,key=settings.INFORMIX_DEBUG,earl=EARL)
