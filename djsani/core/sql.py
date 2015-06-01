992e4f7527c5a4b724dd868ebefb7f25847ba242
Bender <plungerman@gmail.com>
2015-05-31 20:01:39 -0500

    added  waiver IDs and manager id for use on dashboard, along with their respective joins.

--- djsani/core/sql.py
+++ djsani/core/sql.py
@@ -7,8 +7,10 @@ STUDENTS_ALPHA = """
 SELECT UNIQUE
     id_rec.lastname, id_rec.firstname, id_rec.id,
     profile_rec.birth_date,
+    cc_student_medical_manager.id as manid,
     cc_student_medical_manager.athlete,
     cc_student_medical_manager.sitrep,
+    cc_student_medical_manager.emergency_contact,
     cc_student_medical_manager.sports,
     cc_student_medical_manager.cc_student_medical_history,
     cc_student_medical_manager.cc_student_health_insurance,
@@ -47,16 +49,6 @@ WHERE
     AND stu_acad_rec.reg_hrs    >   0
 """.format(START_DATE,START_DATE)
 
-GROUP_BY = """
-GROUP BY
-    lastname, firstname, id_rec.id, athlete, sports,
-    cc_student_medical_history, cc_student_health_insurance,
-    cc_student_meni_waiver, cc_student_immunization,
-    cc_athlete_medical_history, cc_athlete_privacy_waiver,
-    cc_athlete_reporting_waiver, cc_athlete_risk_waiver,
-    cc_athlete_sicklecell_waiver, waive, proof, results
-"""
-
 STUDENT_VITALS = """
 SELECT
     UNIQUE
@@ -76,6 +68,7 @@ SELECT
     id_rec.addr_line1, id_rec.addr_line2, id_rec.city, id_rec.st,
     id_rec.zip, id_rec.ctry, id_rec.phone, cvid_rec.ldap_name,
     adm_rec.plan_enr_sess,adm_rec.plan_enr_yr,
+    cc_student_medical_manager.id as manid,
     cc_student_medical_manager.athlete, cc_student_medical_manager.sports,
     cc_student_medical_manager.cc_student_immunization,
     cc_student_medical_manager.cc_student_meni_waiver,
@@ -83,13 +76,18 @@ SELECT
     cc_student_medical_manager.cc_athlete_reporting_waiver,
     cc_student_medical_manager.cc_athlete_risk_waiver,
     cc_student_medical_manager.cc_athlete_sicklecell_waiver,
+    cc_athlete_sicklecell_waiver.id as sicklecell_id,
     cc_athlete_sicklecell_waiver.updated_at,
     cc_athlete_sicklecell_waiver.waive,
     cc_athlete_sicklecell_waiver.proof,
     cc_athlete_sicklecell_waiver.results,
+    cc_athlete_privacy_waiver.id as privacy_id,
     cc_athlete_privacy_waiver.news_media,
     cc_athlete_privacy_waiver.parents_guardians,
     cc_athlete_privacy_waiver.disclose_records,
+    cc_student_meni_waiver.id as menid,
+    cc_athlete_risk_waiver.id as risk_id,
+    cc_athlete_reporting_waiver.id as reporting_id,
     profile_rec.birth_date,
     profile_rec.sex,
     mobile_rec.phone as mobile
@@ -102,9 +100,17 @@ LEFT JOIN
 LEFT JOIN
     cc_student_medical_manager ON id_rec.id = cc_student_medical_manager.college_id
 LEFT JOIN
-    cc_athlete_sicklecell_waiver ON cc_student_medical_manager.id = cc_athlete_sicklecell_waiver.manager_id
+    cc_student_meni_waiver ON cc_student_medical_manager.id = cc_student_meni_waiver.manager_id
+LEFT JOIN
+    cc_athlete_sicklecell_waiver ON id_rec.id = cc_athlete_sicklecell_waiver.college_id
+    AND
+        (cc_athlete_sicklecell_waiver.proof = 1 or cc_athlete_sicklecell_waiver.created_at > "{}")
 LEFT JOIN
     cc_athlete_privacy_waiver ON cc_student_medical_manager.id = cc_athlete_privacy_waiver.manager_id
+LEFT JOIN
+    cc_athlete_risk_waiver ON cc_student_medical_manager.id = cc_athlete_risk_waiver.manager_id
+LEFT JOIN
+    cc_athlete_reporting_waiver ON cc_student_medical_manager.id = cc_athlete_reporting_waiver.manager_id
 LEFT JOIN
     profile_rec  ON  id_rec.id = profile_rec.id
 LEFT JOIN
@@ -112,7 +118,7 @@ LEFT JOIN
 LEFT JOIN
     aa_rec as mobile_rec on
     (id_rec.id = mobile_rec.id AND mobile_rec.aa = "ENS")
-""".format(START_DATE, START_DATE, START_DATE)
+""".format(START_DATE)
 
 ACADEMIC_YEAR = """
 SELECT
