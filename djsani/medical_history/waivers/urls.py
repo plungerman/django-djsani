37bf81fccdf1f1c1a72ead43cad4e6d7fa2e8acb
Bender <plungerman@gmail.com>
2016-02-02 10:41:54 -0600

    added redirect as default when no stype and type are provided in order to prevent a 500 server error

--- djsani/medical_history/waivers/urls.py
+++ djsani/medical_history/waivers/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView
+from django.views.generic import RedirectView
 
 urlpatterns = patterns('djsani.medical_history.waivers.views',
     # generic waiver successfull submission redirect
@@ -14,5 +16,9 @@ urlpatterns = patterns('djsani.medical_history.waivers.views',
     url(
         r'^(?P<stype>[a-zA-Z0-9_-]+)/(?P<wtype>[a-zA-Z0-9_-]+)/$',
         'form', name="waiver_form"
+    ),
+    url(
+        r'^$',
+        RedirectView.as_view(url=reverse_lazy("home"))
     ),
 )
