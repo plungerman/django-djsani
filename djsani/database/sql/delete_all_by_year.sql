0cfc7947f4b1ea7d0f0cfcfc7ac9fa79c225f6cd
Bender <plungerman@gmail.com>
2016-03-15 10:05:09 -0500

    delete all student profiles that were created after a certain date

---
+++ djsani/database/sql/delete_all_by_year.sql
@@ -0,0 +1,53 @@
+DELETE
+FROM
+    train:cc_athlete_medical_history
+WHERE
+    created_at > "2015-05-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_athlete_privacy_waiver
+WHERE
+    created_at > "2015-05-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_athlete_reporting_waiver
+WHERE
+    created_at > "2015-05-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_athlete_risk_waiver
+WHERE
+    created_at > "2015-05-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_athlete_sicklecell_waiver
+WHERE
+    created_at > "2015-05-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_student_health_insurance
+WHERE
+    created_at > "2015-04-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_student_medical_history
+WHERE
+    created_at > "2015-04-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_student_meni_waiver
+WHERE
+    created_at > "2015-04-01 00:00:00";
+
+DELETE
+FROM
+    train:cc_student_medical_manager
+WHERE
+    created_at > "2015-04-01 00:00:00";
