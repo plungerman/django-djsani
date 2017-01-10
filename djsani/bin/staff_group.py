329b10b4c30487eb1d3a827d0a073900fccc1232
plungerman <plungerman@gmail.com>
2017-01-10 11:32:44 -0600

    now using django1.9

--- djsani/bin/staff_group.py
+++ djsani/bin/staff_group.py
@@ -6,7 +6,7 @@ import django
 sys.path.append('/usr/local/lib/python2.7/dist-packages/')
 sys.path.append('/usr/lib/python2.7/dist-packages/')
 sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/data2/django_1.7/')
+sys.path.append('/data2/django_1.9/')
 sys.path.append('/data2/django_projects/')
 sys.path.append('/data2/django_third/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djsani.settings")
