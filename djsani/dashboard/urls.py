9479458d940ed113d5597c8da5bc5cb0388f51e6
plungerman <plungerman@gmail.com>
2017-04-21 15:41:29 -0500

    new url infrastructure in preparation for django 1.11

--- djsani/dashboard/urls.py
+++ djsani/dashboard/urls.py
@@ -1,7 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns('djsani.dashboard.views',
+from djsani.dashboard import views
+
+urlpatterns = [
     url(
         r'^success/$',
         TemplateView.as_view(
@@ -12,42 +14,42 @@ urlpatterns = patterns('djsani.dashboard.views',
     # generic send mail functions
     url(
         r'^send-mail/$',
-        'sendmail', name="sendmail"
+        views.sendmail, name="sendmail"
     ),
     # ajax communication to paint the panels
     url(
         r'^panels/$',
-        'panels', name="dashboard_panels"
+        views.panels, name="dashboard_panels"
     ),
     # ajax returns students because using home view is a
     # pain in the ass with security involved & spinner
     url(
         r'^get-students/$',
-        'get_students', name="get_students"
+        views.get_students, name="get_students"
     ),
     # simple ID search
     url(
         r'^student/$',
-        'student_detail', name="student_search"
+        views.student_detail, name="student_search"
     ),
     # name search
     url(
         r'^student/search/$',
-        'advanced_search', name="advanced_search"
+        views.advanced_search, name="advanced_search"
     ),
     # student detail
     url(
         r'^student/(?P<cid>\d+)/$',
-        'student_detail', name="student_detail"
+        views.student_detail, name="student_detail"
     ),
     # student detail content specific
     url(
         r'^student/(?P<cid>\d+)/(?P<medium>[-\w]+)/(?P<content>[-\w]+)/$',
-        'student_detail',
+        views.student_detail,
         name="student_detail_medium"
     ),
     # home
     url(
-        r'^$', 'home', name="dashboard_home"
+        r'^$', views.home, name="dashboard_home"
     ),
-)
+]
