bbf37e22701990d4269d8c79b39197acff62e8d4
plungerman <plungerman@gmail.com>
2018-08-15 10:52:31 -0500

    updates for incoming freshman and transfer codes

--- djsani/database/sql/freshman_labels.sql
+++ djsani/database/sql/freshman_labels.sql
@@ -1,5 +1,4 @@
-SELECT
-    UNIQUE
+SELECT UNIQUE
     CASE
         WHEN
             NVL(UPPER(stu_serv_rec.bldg), '') = 'CMTR'
@@ -19,21 +18,24 @@ SELECT
 FROM
     id_rec
 INNER JOIN
-    prog_enr_rec ON  id_rec.id = prog_enr_rec.id
+    prog_enr_rec ON id_rec.id = prog_enr_rec.id
 LEFT JOIN
-    stu_acad_rec    ON  id_rec.id   =   stu_acad_rec.id
+    stu_acad_rec ON id_rec.id = stu_acad_rec.id
 LEFT JOIN
-    stu_serv_rec    ON  id_rec.id   =   stu_serv_rec.id
+    stu_serv_rec ON id_rec.id = stu_serv_rec.id
 LEFT JOIN
-    profile_rec  ON  id_rec.id = profile_rec.id
+    profile_rec  ON id_rec.id = profile_rec.id
 WHERE
     prog_enr_rec.subprog NOT IN  ("UWPK","RSBD","SLS","PARA","MSW","KUSD","ENRM","CONF","CHWK")
 AND
     prog_enr_rec.lv_date IS  NULL
 AND
-    stu_acad_rec.sess IN  ("RA","RC","AM","GC","PC","TC")
-
-        AND stu_serv_rec.yr = "2016"
-        AND stu_serv_rec.sess = "RA"
-        AND prog_enr_rec.cl IN ("FN","FF","FR","UT")
-        ORDER BY lastname
+    stu_acad_rec.sess IN  ("RA","RC","AM","GC","PC","TC","GD","GA","GC")
+AND
+    stu_serv_rec.yr = "2018"
+AND
+    stu_serv_rec.sess = "RA"
+AND
+    prog_enr_rec.cl IN ("FN","FF","FR","UT","PF","PN")
+ORDER BY
+    lastname
