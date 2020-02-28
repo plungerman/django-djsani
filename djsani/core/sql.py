214d7b36d07e2b3bba90c17fbb40f7d4382d9d74
plungerman <plungerman@gmail.com>
2020-02-28 14:22:33 -0600

    sql incantation for obtain sports in which students participate

--- djsani/core/sql.py
+++ djsani/core/sql.py
@@ -219,3 +219,27 @@ LEFT JOIN
 WHERE
     cc_student_medical_manager.college_id=
 """
+
+
+sports = """
+SELECT
+   TRIM(IT.invl) AS sport_code,
+   TRIM(IT.txt) AS sport_name,
+   INR.id AS id,
+   INR.beg_date,
+   INR.end_date
+FROM
+   invl_table IT
+INNER JOIN
+    involve_rec INR
+ON
+    TRIM(IT.invl) = TRIM(INR.invl)
+AND
+    IT.sanc_sport = 'Y'
+WHERE
+   TODAY BETWEEN IT.active_date AND NVL(IT.inactive_date, TODAY)
+AND
+   TODAY < NVL(INR.end_date, TODAY)
+AND
+   INR.id =
+"""
