403d8da51f5e9a4cf1bda3d11189adf80cfc9c2b
plungerman <plungerman@gmail.com>
2023-06-06 18:47:11 -0500

    added search on health insurance objects by user values

--- djsani/insurance/admin.py
+++ djsani/insurance/admin.py
@@ -8,6 +8,12 @@ class StudentHealthInsuranceAdmin(admin.ModelAdmin):
     """Data model class for student health insurance."""
 
     raw_id_fields = ('user', 'manager')
+    search_fields = (
+        'user__last_name',
+        'user__first_name',
+        'user__username',
+        'user__id',
+    )
 
 
 admin.site.register(StudentHealthInsurance, StudentHealthInsuranceAdmin)
